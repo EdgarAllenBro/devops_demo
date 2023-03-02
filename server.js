44ae6377d8f064e5f704cdae6105621502eee4b9
Edgar_Allen_Bro <cjgrieder@gmail.com>
2023-03-02 07:57:29 -0700

    cleared rollbar

--- server.js
+++ server.js
@@ -2,14 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-const Rollbar = require('rollbar')
-const rollbar = new Rollbar({
-    accessToken: 'f1ab85f478df41e2b615c0cc6d33a832',
-    captureUncaught: true,
-    captureUnhandledRejections: true,
-})
 
-rollbar.log('hello rollbar')
 
 app.use(express.json())
 
@@ -21,7 +14,7 @@ app.get('/', (req, res) => {
 
 app.get('/api/students', (req, res) => {
     res.status(200).send(students)
-    rollbar.info('student List was requested and sent')
+
 })
 
 app.post('/api/students', (req, res) => {
@@ -35,13 +28,10 @@ app.post('/api/students', (req, res) => {
        if (index === -1 && name !== '') {
            students.push(name)
            res.status(200).send(students)
-           rollbar.critical('NEW STUDENT')
        } else if (name === ''){
            res.status(400).send('You must enter a name.')
-           rollbar.error('attempted empty student name')
        } else {
            res.status(400).send('That student already exists.')
-           rollbar.warning('attempted duplicate student')
        }
    } catch (err) {
        console.log(err)
