ccc19e6ed71f24fbe013a3ff8f677830a4c6a4d9
Edgar_Allen_Bro <cjgrieder@gmail.com>
2023-03-02 10:24:55 -0700

    added rollbar error checks

--- server.js
+++ server.js
@@ -2,18 +2,26 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'd332ee6d8a424f6591776887a98b4a11',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
 
 app.use(express.json())
 
 const students = ['Jimmy', 'Timothy', 'Jimothy']
+rollbar.log('site is live')
 
 app.get('/', (req, res) => {
+    rollbar.log('site visited')
     res.sendFile(path.join(__dirname, '/index.html'))
 })
 
 app.get('/api/students', (req, res) => {
     res.status(200).send(students)
+    rollbar.info('student list is sent')
 
 })
 
@@ -28,10 +36,13 @@ app.post('/api/students', (req, res) => {
        if (index === -1 && name !== '') {
            students.push(name)
            res.status(200).send(students)
+           rollbar.info('student added successfully')
        } else if (name === ''){
            res.status(400).send('You must enter a name.')
+           rollbar.error('attempted to add blank student name')
        } else {
            res.status(400).send('That student already exists.')
+           rollbar.warning('attempted dupe student')
        }
    } catch (err) {
        console.log(err)
